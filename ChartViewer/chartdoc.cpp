586aac62c1daa751a5aaab3a6da4755cbbdf4e24
Predrag Antanasijevic <antanas686@gmail.com>
2017-12-07 19:59:29 +0100

    Updated drawing
    Still need to move ChartDoc object to MainWindow.

--- ChartViewer/chartdoc.cpp
+++ ChartViewer/chartdoc.cpp
@@ -49,11 +49,18 @@ void ChartDoc::saveChartToFile(QString filePath)
 
 void ChartDoc::Draw(QPainter& p)
 {
+    if(m_points.size() == 0)
+        return;
     float max = getMaxValue();
     for(int i=0;i<m_points.size();i++){
         float percent = m_points[i]->GetValue()/max;
         m_points[i]->DrawPoint(p, i, percent);
     }
+    for(int i=0;i<11;i++){
+        p.drawLine(25, i*40+50, 50, i*40+50);
+        p.drawText(5, i*40+40, QString::number(max - max*i/10));
+    }
+
 }
 
 float ChartDoc::getMaxValue()
